38cdff19f8467ed171129ce38fcad852dd8edd91
coon <coon@c-base.org>
2016-04-25 21:49:30 +0200

    add nutrition test

--- myFood/tests.py
+++ myFood/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.db.utils import IntegrityError
 
-# Create your tests here.
+from .models import Food, Nutrition, Composition
+
+class FoodDataBaseTests(TestCase):
+  def test_if_nutrition_can_only_be_assigned_once_per_food(self):
+    """
+    It must not be possible to assign a specific nutrition more
+    than once to the same food.
+    """
+    f = Food(name="Club Mate")
+    f.save()
+
+    n1 = Nutrition.objects.create(name="fat")
+    n2 = Nutrition.objects.create(name="carbohydrate")
+    n1.save()
+    n2.save()
+    
+    c = Composition(food=f, nutrition=n1, ammount=20)
+    c.save()
+
+    c = Composition(food=f, nutrition=n2, ammount=20)
+    c.save()
+
+    c = Composition(food=f, nutrition=n1, ammount=20)
+    with self.assertRaises(IntegrityError):
+      c.save()
+
