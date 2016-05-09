1e00a3e3ea4862a2b3703e764d6bb61136369dff
coon42 <coon@c-base.org>
2016-05-10 00:50:52 +0200

    calulate total calories in template

--- myFood/views.py
+++ myFood/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
 from django.contrib import admin
 from .models import CompositionInline, PackagesInline
-from .models import Meal
+from .models import Meal, Nutrition
 from django.views import generic
 from django.utils import timezone
-from django.db.models import Sum
 
 def Index(request):
   meals_list = Meal.objects.all().filter(date = timezone.now())
+  nutrition_names = [n.name for n in Nutrition.objects.all()]
+
+  for nutrition in nutrition_names:
+    setattr(meals_list, "kcal_" + nutrition, 0)
 
   for meal in meals_list:
+    for nutrition in nutrition_names:
+      setattr(meal, "kcal_" + nutrition, "N/A")
+
     for nutrition, ammount in meal.kcals_of_nutritions().items():
       setattr(meal, "kcal_" + nutrition, ammount)
+      setattr(meals_list, "kcal_" + nutrition, getattr(meals_list, "kcal_" + nutrition) + ammount)
+
+  meals_list.kcal_total = sum([getattr(meals_list, "kcal_" + n) for n in nutrition_names])
 
   context = {
     'meals_list': meals_list,
